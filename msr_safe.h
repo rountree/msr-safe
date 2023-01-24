9359552e2bb9b356a0ec1bca9445441c8d8a143e
Barry <rountree4@llnl.gov>
2023-01-24 08:40:03 -0800

    Adds new _ex structs for batchs arrays and ops.
    This plays stupid struct layout games.  The first __u32 of
    the incoming ioctl pointer will be interpreted as a pointer
    to msr_batch_array if nonzero and a pointer to msr_batch_array_ex
    if 0.  The latter will then compare the kernel module version
    with the requested version and proceed only if they match.

--- msr_safe.h
+++ msr_safe.h
@@ -28,12 +28,62 @@ struct msr_batch_op
     __u64 wmask;   // Out: Write mask applied to wrmsr
 };
 
-struct msr_batch_array
-{
-    __u32 numops;             // In: # of operations in operations array
-    struct msr_batch_op *ops; // In: Array[numops] of operations
+enum msr_commands { // LIMIT TO __u16
+    MSR_OP_WR   = 0x00, // Write the contents of msrdata to the msr
+    MSR_OP_RD   = 0x01, // After the write command (if any), 
+                        //   read the contents of the msr into msrdata
+    MSR_OP_POLL = 0x02, // After the read and write commands, if any,
+                        //   continuously read the value of the msr into
+                        //   msrdata until it changes, then write the new value
+                        //   into msrpolldata
+    MSR_OP_AP_0 = 0x04, // Read MSR_APERF into aperf0 before anything else
+    MSR_OP_AP_1 = 0x08, // Read MSR_APERF into aperf1 after everything else
+    MSR_OP_MP_0 = 0x10, // Read MSR_MPERF into mperf0 before everything 
+                        //   (except MSR_APERF)
+    MSR_OP_MP_1 = 0x20, // Read MSR_MPERF into mperf1 after everything 
+                        //   (except MSR_APERF)
+    MAX_OP = 0x7FFF     // Might be able to relax this 
+
 };
 
+
+
+struct msr_batch_op_ex
+{
+    __u16 cpu;          // In: CPU to execute {rd/wr}msr instruction
+    __u16 cmd;          // In: See msr_commands, can be or'ed together.
+    __s32 err;          // Out: set if error occurred with this operation
+    __u32 msr;          // In: MSR Address to perform operation
+    __u64 msrdata;      // In/Out: Input/Result to/from operation
+    __u64 msrpolldata;  // Out:  New msr value
+    __u64 aperf0;       // Out:  Timestamp value prior to the op
+    __u64 aperf1;       // Out:  Timestamp value after the op
+    __u64 mperf0;       // Out:  Timestamp value prior to the op
+    __u64 mperf1;       // Out:  Timestamp value after the op
+    __u64 wmask;        // Out: Write mask applied to wrmsr
+    __u16 err_idx;      // Out: To which operation does the error apply
+    __u16 valid_idx;    // Out: Bitwise-and with cmd for a quick check to see
+                        //   which of these fields contain useful info
+                        //   For example, if cmd has MSR_OP_RD and MSR_OP_POLL
+                        //   set, and MSR_OP_READ succeeds but MSR_OP_POLL
+                        //   fails without invalidating the contents of 
+                        //   msrdata, then the MSR_OP_READ bit of valid_idx
+                        //   will be 1 and the MSR_OP_POLL bit will be 0.
+};
+
+struct msr_batch_array
+{
+    __u32 numops;               // In: # of operations in operations array
+    struct msr_batch_op *ops;   // In: Array[numops] of operations
+};
+
+struct msr_batch_array_ex
+{
+    __u32 always_zero;          // In: Set to 0 to use new API
+    struct msr_batch_op_ex *ops_ex;// In: Array[numops] of operations
+    __u32 numops_ex;            // In: # of operations in operations array
+    __u16 version_requested;    // In: (MSR_SAFE_VER_MAJ<<8) | MSR_SAFE_VER_MIN
+};
 #define X86_IOC_MSR_BATCH   _IOWR('c', 0xA2, struct msr_batch_array)
 
 #endif
